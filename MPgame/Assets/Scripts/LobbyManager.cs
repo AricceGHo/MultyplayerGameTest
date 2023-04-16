6acef1718a999ad8372e131fb3222e8a43ab4262
AricceGHo <44861119+AricceGHo@users.noreply.github.com>
2023-04-16 22:22:24 +0300

    Начальный
    Создание проекта и первоначальные настройки

---
+++ MPgame/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text LogText;
+    [SerializeField] InputField nickInputField;
+    [SerializeField] InputField roomNameInputField;
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(1, 9999); //Никнейм
+        Log("Имя игрока: " + PhotonNetwork.NickName);
+        PhotonNetwork.AutomaticallySyncScene = true; //Автопереключение сцены
+        PhotonNetwork.GameVersion = "1"; //Версия игры
+        PhotonNetwork.ConnectUsingSettings(); //Подключается к серверу Photon
+    }
+    void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
+    }
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(roomNameInputField.text, new Photon.Realtime.RoomOptions { MaxPlayers = 15 });
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(roomNameInputField.text);
+    }
+    public override void OnConnectedToMaster()
+    {
+        Log("Подключились к серверу Photon");
+    }
+    public override void OnJoinedRoom()
+    {
+        Log("Вошли в комнату");
+        PhotonNetwork.LoadLevel("Room");
+    }
+    public void ChangeName()
+    {
+        PhotonNetwork.NickName = nickInputField.text;
+        Log("Новое имя игрока: " + PhotonNetwork.NickName);
+    }
+}
---
+++ MPgame/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,82 @@
+﻿using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using System.Collections.Generic;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text ChatText;
+    [SerializeField] InputField InputText;
+    void Log(string message)
+    {
+        Debug.Log(message);
+        ChatText.text += "\n";
+        ChatText.text += message;
+    }
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    //События комнаты
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        Log(newPlayer.NickName + " зашел в комнату");
+        playersList.Add(newPlayer.NickName);
+        RefreshPlayers();
+    }
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        Log(otherPlayer.NickName + " вышел из комнаты");
+        playersList.Remove(otherPlayer.NickName);
+        RefreshPlayers();
+    }
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //RPC методы
+    [PunRPC]
+    public void ShowMessage(string message, PhotonMessageInfo info)
+    {
+        ChatText.text += "\n";
+        ChatText.text += message;
+    }
+    public void Send()
+    {
+        if (string.IsNullOrWhiteSpace(InputText.text)) { return; }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            photonView.RPC("ShowMessage", RpcTarget.All, PhotonNetwork.NickName + ": " + InputText.text);
+            InputText.text = string.Empty;
+        }
+    }
+
+
+
+    [SerializeField] List<string> playersList;
+    [SerializeField] Text PlayersText;
+    private void Start()
+    {
+        playersList.Add(PhotonNetwork.NickName);
+        RefreshPlayers();
+    }
+
+    void RefreshPlayers()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC("ShowPlayers", RpcTarget.All, "Игроки" + "\n" + string.Join("\n", playersList));
+        }
+    }
+
+    [PunRPC]
+    public void ShowPlayers(string players)
+    {
+        PlayersText.text = players;
+    }
+
+
+}
